992d295c222d8731730e42a17ccd692a53d7fb93
Trent Hauck <trent@trenthauck.com>
2020-10-23 21:11:25 -0700

    test: adds test setup

---
+++ tests/cli.rs
@@ -0,0 +1,15 @@
+// (c) Copyright 2020 Trent Hauck
+// All Rights Reserved
+
+use assert_cmd::prelude::*;
+use std::process::Command;
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("brrrr")?;
+
+    cmd.arg("--version");
+    cmd.assert().success();
+
+    Ok(())
+}
