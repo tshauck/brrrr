2c60b12e09163fd181851f963175b428e9cae32d
Trent Hauck <trent@trenthauck.com>
2020-10-25 20:02:12 -0700

    bump: version 0.5.0 → 0.6.0

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ enum GeneratorChoice {
     name = "brrrr",
     about = "Convert biology and chemistry domain formats into common formats.",
     author = "Trent Hauck <trent@trenthauck.com>",
-    version = "0.5.0"
+    version = "0.6.0"
 )]
 enum Brrrr {
     #[clap(name = "fa2jsonl", about = "Converts a FASTA input to jsonl.")]
