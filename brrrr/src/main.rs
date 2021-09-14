24f9668a2a67b84fffd5e3ad6dd105ecf232a36a
Trent Hauck <trent@trenthauck.com>
2021-09-13 18:12:18 -0700

    bump: version 0.9.0 → 0.9.1

--- brrrr/src/main.rs
+++ brrrr/src/main.rs
@@ -30,7 +30,7 @@ enum GeneratorChoice {
     name = "brrrr",
     about = "Commandline utilities for modern biology and chemistry informatics.",
     author = "Trent Hauck <trent@trenthauck.com>",
-    version = "0.9.0"
+    version = "0.9.1"
 )]
 enum Brrrr {
     #[clap(name = "fa2pq", about = "Converts a FASTA input to parquet.")]
